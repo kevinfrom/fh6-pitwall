74329f2e80173c7ff7f26e15fecdb3c29edc221a
Kevin From <kevin@kevinfrom.dk>
2026-06-08 12:05:25 +0200

    created varlock env schema

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      fh6UdpPort: '' // NUXT_PUBLIC_FH6_UDP_PORT
+    }
+  }
 })
